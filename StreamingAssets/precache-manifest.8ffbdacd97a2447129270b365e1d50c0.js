dcc57c51aa486742ed323529ed4281614f964e4d
Subhradeep <subhradeep@micronixsystem.com>
2022-11-17 16:10:12 +0530

    updst

---
+++ StreamingAssets/precache-manifest.8ffbdacd97a2447129270b365e1d50c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5437d542a5a73cce7f2",
+    "url": "./static/css/main.af12aeb4.chunk.css"
+  },
+  {
+    "revision": "c5437d542a5a73cce7f2",
+    "url": "./static/js/main.c5437d54.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "0ec96b1194d7be2e7d9d",
+    "url": "./static/js/2.0ec96b11.chunk.js"
+  },
+  {
+    "revision": "1ad5ce9801c6d01453b0a7ad4bc6c5f5",
+    "url": "./static/media/search.1ad5ce98.svg"
+  },
+  {
+    "revision": "aca4051c4e79a8a8d824959f161335a2",
+    "url": "./static/media/logo-geogram.aca4051c.svg"
+  },
+  {
+    "revision": "fba0429a260deaa84aee5db5a8735d47",
+    "url": "./static/media/truss.fba0429a.jpg"
+  },
+  {
+    "revision": "7f5c47036bc0666c529804c2967f5b01",
+    "url": "./static/media/chair.7f5c4703.jpg"
+  },
+  {
+    "revision": "1d9a793c6dc1692e45ec527f92fdb646",
+    "url": "./static/media/image.1d9a793c.png"
+  },
+  {
+    "revision": "7c78980ea790ccc4beb42ac461d1456e",
+    "url": "./static/media/table.7c78980e.jpg"
+  },
+  {
+    "revision": "be5d9f38fa44f6f4f2e87a8065a710bd",
+    "url": "./static/media/video.be5d9f38.png"
+  },
+  {
+    "revision": "0f3ddf4d94cdea2118ba51122f4422f5",
+    "url": "./static/media/pipeAndDrape.0f3ddf4d.jpg"
+  },
+  {
+    "revision": "82a235eef8968987e0628e531273ab40",
+    "url": "./static/media/geogramScreenshot.82a235ee.png"
+  },
+  {
+    "revision": "dd03ad933987b9122b6c89e3c587b980",
+    "url": "./index.html"
+  }
+];
